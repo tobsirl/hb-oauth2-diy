3f4f3f8e191eb23a31b308bc89a977c592525a21
Paul Tobin <tobster111@gmail.com>
2022-03-27 14:40:13 +0100

    Updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  resources :oauth_clients
+  match '/oauth/test_request',  via: [:get], :to => 'oauth#test_request',     :as => :test_request
+  match '/oauth/token',         via: [:post], :to => 'oauth#token',           :as => :token
+  match '/oauth/access_token',  via: [:get], :to => 'oauth#access_token',     :as => :access_token
+  match '/oauth/request_token', via: [:get], :to => 'oauth#request_token',    :as => :request_token
+  match '/oauth/authorize',     via: [:get, :post], :to => 'oauth#authorize', :as => :authorize
+  match '/oauth',               via: [:get], :to => 'oauth#index',            :as => :oauth
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root :to => "oauth_clients#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
