37d54a318b2ef436fbcc2e436513f7b7f650b842
Paul Tobin <tobster111@gmail.com>
2022-04-17 14:03:53 +0100

    oauth token and application

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require 'oauth/rack/oauth_filter'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +11,7 @@ module HbOauth2Provider
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.middleware.use OAuth::Rack::OAuthFilter
 
     # Configuration for the application, engines, and railties goes here.
     #
