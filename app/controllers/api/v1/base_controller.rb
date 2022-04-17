38d263b7bf553346e324cbe7c393f00cc0d4fb2c
Paul Tobin <tobster111@gmail.com>
2022-04-17 14:00:43 +0100

    base and data setup

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,4 @@
 class Api::V1::BaseController < ApplicationController
+  oauthenticate :interactive=>false
+  respond_to :json, :xml
 end
--- app/controllers/api/v1/data_controller.rb
+++ app/controllers/api/v1/data_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::DataController < ApplicationController
+  def show
+    respond_with ({:super_secret => "oauth_data"})
+ end
 end
